518a843ec54cce568271c5f8a80b04271793ac0b
bartosz <bartosz.lysniewski@gmail.com>
2022-06-10 02:09:34 +0200

    Web

---
+++ src/components/ContentWrapper/ContentWrapper.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-content: space-between;
+`;
